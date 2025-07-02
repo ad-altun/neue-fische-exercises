dc34b0800c3791b2b954d2f6c60b27fcb33c1580
ad-altun <adaltun@web.de>
2025-07-02 14:25:20 +0200

    boilerplate codes added: getter, setter, and @override(s)

--- classes-and-objects-practice/src/main/java/org/example/Car.java
+++ classes-and-objects-practice/src/main/java/org/example/Car.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Car {
     private String brand;
     private String model;
@@ -7,6 +9,13 @@ public class Car {
     private int manufacturingYear;
     private int speed;
 
+    public Car(String brand, String model, String color, int manufacturingYear, int speed) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.manufacturingYear = manufacturingYear;
+        this.speed = speed;
+    }
 
     public void setSpeed(int speed) {
         this.speed = speed;
@@ -21,8 +30,32 @@ public class Car {
     };
 
     public void accelerate(int speed) {
-        this.speed = speed;
+        this.speed = speed + this.speed;
         System.out.println("New speed of the vehicle is " + this.getSpeed() + " km/h.");
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return manufacturingYear == car.manufacturingYear && speed == car.speed &&
+                        Objects.equals(brand, car.brand) && Objects.equals(model, car.model) &&
+                        Objects.equals(color, car.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, color, manufacturingYear, speed);
+    }
+
+        @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", manufacturingYear=" + manufacturingYear +
+                ", speed=" + speed +
+                '}';
+    }
 }
--- classes-and-objects-practice/src/main/java/org/example/Main.java
+++ classes-and-objects-practice/src/main/java/org/example/Main.java
@@ -3,10 +3,29 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Car car = new Car();
+        Car carMazda = new Car("Mazda", "Cx-30", "sand",
+                        2024, 90);
 
-        car.start();
-        car.accelerate(50);
+        Car carMazda2 = new Car("Mazda", "Cx-30", "sand",
+                2024, 90);
+
+        Car carRenault =  new Car("Renault", "Twingo", "blue",
+                        2015, 60);
+
+//        String carMazda = car.toString();
+        System.out.println(carMazda);
+//        carMazda.start();
+//        carMazda.accelerate(50);
+
+        System.out.println();
+
+        System.out.println(carMazda == carMazda2);
+        System.out.println(carMazda.equals(carMazda2));
+        System.out.println(carMazda == carRenault);
+        System.out.println(carMazda.equals(carRenault));
+
+        System.out.println(carMazda);
+        System.out.println(carMazda2);
 
     }
 }
