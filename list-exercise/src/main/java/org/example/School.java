4676ed74a1a9b101e76fe3739496401be8b26101
ad-altun <adaltun@web.de>
2025-07-07 15:16:56 +0200

    - add and get students as hashmap as well

--- list-exercise/src/main/java/org/example/School.java
+++ list-exercise/src/main/java/org/example/School.java
@@ -1,19 +1,27 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class School {
 
     List<Student> students = new ArrayList<>();
+    Map<String, String> studentsMap = new HashMap<>();
+
+
 
     public void getStudents() {
         System.out.println("The list of students is: '\n'" + students);
+        System.out.println("The list of students in map is: `\n'" + studentsMap);
     }
 
     public void addStudent(Student student) {
         students.add(student);
+        studentsMap.put(student.getStudentId(), student.getFirstName() + " " + student.getLastName());
         System.out.println("Added student " + student);
+        System.out.println("Added student in map " + student);
     }
 
     public Student findStudentByStudentId(String studentId) {
