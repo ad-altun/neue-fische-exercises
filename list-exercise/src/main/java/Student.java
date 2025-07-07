e7821781777940771d7d705c6447c7708f8d6bde
ad-altun <adaltun@web.de>
2025-07-07 10:19:13 +0200

    add Student class

---
+++ list-exercise/src/main/java/Student.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    // properties - fields
+    private String firstName;
+    private String lastName;
+    private String studentId;
+
+    // Student class constructor
+    public Student(String firstName, String lastName, String studentID){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentID;
+    }
+
+    // getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    // override the toString() method
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", studentId='" + studentId + '\'' +
+                '}';
+    }
+}
