474a42fc73ea0bfb2103efa3f4e9adafe2b9a2bb
ad-altun <adaltun@web.de>
2025-07-07 09:54:45 +0200

    - added control for negative numbers - still working on floating numbers

--- fibonacci-with-tdd/src/main/java/org/example/Main.java
+++ fibonacci-with-tdd/src/main/java/org/example/Main.java
@@ -8,8 +8,9 @@ public class Main {
         System.out.println("Enter a digital number: ");
 
         int digitalNumber = scanner.nextInt();
+        handleFloating(digitalNumber);
 
-        fibonacciNumber(digitalNumber);
+        fibonacciNumber(handleNegative(handleFloating(digitalNumber)));
 
     }
 
@@ -20,7 +21,7 @@ public class Main {
 
         int fib = 0;
 
-        if ( n == 1 ) fib = 1;
+        if ( n == 1 || n == -1 ) fib = 1;
         else {
             for(int i = 2; i <= n; i++){
                 fib = previous + current;
@@ -32,6 +33,17 @@ public class Main {
         System.out.println("Fibonacci number for " + n + " is: " + fib + ".");
 
     }
+
+    public static int handleNegative(int n){
+        if ( n<0 ) return -1 * n;
+            else return n;
+    };
+
+    public static int handleFloating(float n){
+        System.out.println("Floating number: " + n);
+        return (int) n;
+    }
+
 }
 
 
