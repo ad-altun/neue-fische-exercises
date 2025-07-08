0ddf05df96d03563dccac84fbae51ce7768169c7
ad-altun <adaltun@web.de>
2025-07-08 14:28:08 +0200

    bank-service: add Record Client and Class Account

---
+++ bank-service-exercise/src/main/java/org/example/Account.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private String accountNumber;
+
+    BigDecimal balance = new BigDecimal("0");
+    Client client;
+
+}
