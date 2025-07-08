d871853b48e63d165e412f7cced342522d60f47a
ad-altun <adaltun@web.de>
2025-07-08 10:56:35 +0200

    - add Animal Record.  - call record methods from Main class

---
+++ record-exercise/src/main/java/org/example/Animal.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public record Animal(String animalId, String name, String species, int age) {
+
+}
--- record-exercise/src/main/java/org/example/Main.java
+++ record-exercise/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        // go to next line before printing anything
+        System.out.println();
+        System.out.println();
+
+        Animal dog1 = new Animal("1-a", "Pablo", "bulldog", 3);
+        Animal dog2 = new Animal("1-b", "Diego", "english bulldog", 7);
+        Animal dog3 = new Animal("1-c", "Ingo", "azawakh", 9);
+        Animal cat1 = new Animal("2-a", "Kitty", "british shorthair", 2);
+        Animal cat2 = new Animal("2-a", "Kitty", "british shorthair", 2);
+
+        List<Animal> animals = new ArrayList<>(List.of(dog1, dog2, dog3, cat1, cat2));
+
+        for (Animal animal : animals) {
+            System.out.println("-> Animal ID " + animal.animalId() +
+                    " is a " + animal.species() + " and its name is " + animal.name() + "."
+                    + '\n' + " " + animal.name() + " is " + animal.age() + " years old." + "\n");
+        }
+
+        System.out.println();
+
+        System.out.println("Is cat1 == cat2: " + (cat1 == cat2));
+        System.out.println("Is cat1.equals(cat2): " + cat1.equals(cat2));
+
+
     }
 }
