31c8d535fe82c47741b310a937cf7b08d63183c8
ad-altun <adaltun@web.de>
2025-07-11 16:15:32 +0200

    stream-exercise: - filter to output only the even numbers

--- stream-exercise/src/main/java/org/example/Main.java
+++ stream-exercise/src/main/java/org/example/Main.java
@@ -1,8 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("\nHello Java!");
+        List<Integer> numbers = Stream.of(8, 4, 1, 9, 2, 10, 6, 7, 5, 3).collect(Collectors
+                .toCollection(ArrayList::new));
+
+        System.out.println("\nThe list consists of: " + numbers);
+
+        //  Step 1: Filter a list of numbers and output only the even numbers.
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(num -> num % 2 == 0 ).toList();
+
+        System.out.println("\nStep-1");
+        System.out.println("The list filtered to contain only even numbers: \n" + evenNumbers);
+
+
+
     }
 }
