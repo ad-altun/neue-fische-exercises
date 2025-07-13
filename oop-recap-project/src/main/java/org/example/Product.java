c5d8d6d8db1ff1f67421cedb62217f1e2a1921de
ad-altun <adaltun@web.de>
2025-07-13 12:44:40 +0200

    oop-recap-project: - remove toString method from Product class

--- oop-recap-project/src/main/java/org/example/Product.java
+++ oop-recap-project/src/main/java/org/example/Product.java
@@ -1,11 +1,4 @@
 package org.example;
 
 public record Product(String productId, String productName) {
-    @Override
-    public String toString() {
-        return "Product{" +
-                "productId='" + productId + '\'' +
-                ", productName='" + productName + '\'' +
-                '}';
-    }
 }
