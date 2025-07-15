4bf91234c564e0db21d23b8cca7e4ee131eb7cdc
ad-altun <adaltun@web.de>
2025-07-15 16:01:09 +0200

    date-time-exercise: output two weeks later

--- date-time-exercise/src/main/java/org/example/Main.java
+++ date-time-exercise/src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +11,12 @@ public class Main {
         // Step-1: output current date and time
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println("\nThe current local date and time: \n" + localDateTime);
+
+        // Step-2:  Add a timespan of 2 weeks to the current date and output the new date
+        LocalDateTime twoWeeksLater = localDateTime.plusWeeks(2);
+        System.out.println("\nTwo weeks later from now: \n" + twoWeeksLater);
+
+
 
     }
 }
