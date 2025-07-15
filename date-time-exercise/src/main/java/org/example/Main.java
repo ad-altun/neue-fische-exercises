fc858e5e242ed011ce3425b785f4f2182f17b213
ad-altun <adaltun@web.de>
2025-07-15 16:18:51 +0200

    date-time-exercise: output if current day is before or after a specified date

--- date-time-exercise/src/main/java/org/example/Main.java
+++ date-time-exercise/src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class Main {
@@ -16,7 +18,21 @@ public class Main {
         LocalDateTime twoWeeksLater = localDateTime.plusWeeks(2);
         System.out.println("\nTwo weeks later from now: \n" + twoWeeksLater);
 
+        // Step 3: Compare the current date with a specified future date and
+        // output whether the current date is before or after the specified date.
+        LocalDate localDate = LocalDate.now();
+        LocalDate futureDate = LocalDate.of(2025, Month.AUGUST, 3);
+
+        System.out.println("\nThe current local date: \n" + localDate);
+        System.out.println("\nThe specified local date: \n" + futureDate);
+
+        if (localDate.isBefore(futureDate)) {
+            System.out.println("-> The current date is before the specified date.");
+        } else {
+            System.out.println("-> The current date is after the specified date.");
+        }
 
 
     }
+
 }
