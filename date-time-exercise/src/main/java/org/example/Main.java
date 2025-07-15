d773ff085ca9c9984fb857fe471eb6743ca18bf8
ad-altun <adaltun@web.de>
2025-07-15 15:24:57 +0200

    date-time-exercise: output current date and time

--- date-time-exercise/src/main/java/org/example/Main.java
+++ date-time-exercise/src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Java!");
+        // Step-1: output current date and time
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("\nThe current local date and time: \n" + localDateTime);
+
     }
 }
